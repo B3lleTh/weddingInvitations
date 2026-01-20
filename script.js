a788b01a825a6e31a9ffe8bca3e58590764612f9
Ghostar <gillomdev@gmail.com>
2026-01-19 23:22:07 -0700

    3rd Look of Design - Close to Final v0.0

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const eventDate = new Date("2026-08-15T18:00:00").getTime();
 
 setInterval(() => {
-  const now = new Date().getTime();
+  const now = Date.now();
   const diff = eventDate - now;
 
-  const d = Math.floor(diff / (1000 * 60 * 60 * 24));
-  const h = Math.floor((diff / (1000 * 60 * 60)) % 24);
-  const m = Math.floor((diff / (1000 * 60)) % 60);
+  const d = Math.floor(diff / 86400000);
+  const h = Math.floor((diff / 3600000) % 24);
+  const m = Math.floor((diff / 60000) % 60);
   const s = Math.floor((diff / 1000) % 60);
 
   days.textContent = d;
